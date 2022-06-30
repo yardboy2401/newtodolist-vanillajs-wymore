a63beef30cca1db2e2438e7505d6ad36f5024869
Jeff Wymore <yardboy2401@gmail.com>
2022-06-30 15:15:00 -0700

    active-list click event now working for task-list(s)

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const newListInput = document.querySelector('[data-new-list-input')
 
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -36,6 +45,9 @@ function renderList() {
         listElement.dataset.listId = list.id
         listElement.classList.add('list-name')
         listElement.innerText = list.name
+        if(list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
         listsContainer.appendChild(listElement)
     })
 }
@@ -46,5 +58,5 @@ function clearElement(element) {
     }
 }
 
-
-renderList()
+saveAndRender()
+// renderList()
